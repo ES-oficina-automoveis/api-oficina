bd1d48d220f0815240665d06a2a3bf26220e4809
airton23 <airtondasf2305@gmail.com>
2024-08-18 18:48:09 -0300

    Testes Unitarios e correcao nos Testes Controller

--- test/controllers/veiculos_controller_test.rb
+++ test/controllers/veiculos_controller_test.rb
@@ -16,12 +16,19 @@ class VeiculosControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create veiculo" do
-    assert_difference("Veiculo.count") do
-      post veiculos_url, params: { veiculo: { ano: @veiculo.ano, chassi: @veiculo.chassi, cor: @veiculo.cor, modelo: @veiculo.modelo, placa: @veiculo.placa, quilometragem: @veiculo.quilometragem } }
+    assert_difference("Veiculo.count", 1) do
+      post veiculos_url, params: { veiculo: { ano: 2024, chassi: 'NEWCHAI1456789345', cor: 'azul', modelo: 'Novo Modelo', placa: 'NEW1234', quilometragem: 5000, cliente_id: 1 } }
     end
-
-    assert_redirected_to veiculo_url(Veiculo.last)
+    assert_response :redirect
+    veiculo = Veiculo.last
+    assert_equal 'NEWCHAI1456789345', veiculo.chassi
+    assert_equal 'azul', veiculo.cor
+    assert_equal 'Novo Modelo', veiculo.modelo
+    assert_equal 'NEW1234', veiculo.placa
+    assert_equal 5000, veiculo.quilometragem
+    assert_equal 1, veiculo.cliente_id
   end
+
 
   test "should show veiculo" do
     get veiculo_url(@veiculo)
@@ -38,9 +45,17 @@ class VeiculosControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to veiculo_url(@veiculo)
   end
 
-  test "should destroy veiculo" do
+  test "should destroy veiculo without atendimentos" do
+    veiculo_to_destroy = veiculos(:tree)
+
+    assert_empty veiculo_to_destroy.atendimentos, "Veículo tem atendimentos associados"
+
     assert_difference("Veiculo.count", -1) do
-      delete veiculo_url(@veiculo)
+      delete veiculo_url(veiculo_to_destroy)
+    end
+
+    assert_raises(ActiveRecord::RecordNotFound) do
+      Veiculo.find(veiculo_to_destroy.id)
     end
 
     assert_redirected_to veiculos_url
--- test/models/administrador_test.rb
+++ test/models/administrador_test.rb
@@ -1,7 +1,36 @@
 require "test_helper"
 
 class AdministradorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @admin = administradors(:one)
+  end
+
+  test "valid administrador" do
+    assert @admin.valid?
+  end
+
+  test "invalid without email" do
+    @admin.email = nil
+    assert_not @admin.valid?, "Administrador is valid without an email"
+    assert_not_nil @admin.errors[:email], "no validation error for email present"
+  end
+
+  test "email must be unique" do
+    duplicate_admin = @admin.dup
+    duplicate_admin.email = @admin.email.upcase
+    @admin.save
+    assert_not duplicate_admin.valid?, "Administrador is valid with duplicate email"
+  end
+
+  test "invalid without password" do
+    @admin.senha = nil
+    assert_not @admin.valid?, "Administrador is valid without a password"
+    assert_not_nil @admin.errors[:senha], "no validation error for senha present"
+  end
+
+  test "password must have at least 6 characters" do
+    @admin.senha = "12345"
+    assert_not @admin.valid?, "Administrador is valid with a short password"
+    assert_not_nil @admin.errors[:senha], "no validation error for short senha"
+  end
 end
